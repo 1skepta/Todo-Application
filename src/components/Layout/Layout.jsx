122975ad0e7079fb51321df83b8237df3dc6959f
1skepta <prospaul992@gmail.com>
2024-12-14 20:53:30 +0000

    creating mobile responsive ui

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import TodoInput from "../TodoInput/TodoInput";
 import TodoList from "../TodoList/TodoList";
+import styles from "./Layout.module.css";
+import mooon from "../../assets/images/icon-moon.svg";
 
 function Layout() {
   return (
-    <div style={{ backgroundColor: "red", width: "100vw" }}>
-      <TodoInput />
-      <TodoList />
+    <div className={styles.background}>
+      <div className={styles.backgroundHeader}>
+        <div className={styles.header}>
+          <h1>T O D O</h1>
+          <img src={mooon} alt="Theme Logo" />
+        </div>
+        <TodoInput />
+      </div>
+      <div>
+        <TodoList />
+      </div>
     </div>
   );
 }
